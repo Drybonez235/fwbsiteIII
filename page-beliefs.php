b3aaa5419968b4dd56eaceb3da4b5a9b309d0647
Jonathan Lewis <jonathan.lewis@welch.edu>
2026-04-11 16:49:48 -0500

    added the page-beliefs.twig

--- page-beliefs.php
+++ page-beliefs.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Template Name: Beliefs Page
- * Description: A template for displaying the church's statement of faith and core beliefs.
- */
-
-
 $context = Timber::context();
 $context['post'] = Timber::get_post(); // Timber 2.x uses get_post() instead of a new Post()
 Timber::render('beliefs-page.twig', $context);
