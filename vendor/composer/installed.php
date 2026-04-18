2eeb0872ea369243fcec63348a3bfd8df7b5cc1e
Jonathan Lewis <jonathan.lewis@welch.edu>
2026-04-18 16:06:18 -0500

    updated the events page UI

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'jonathanlewis/fwbsite-iii',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'd83bcaefea809535047b0423b5eb7e4f56783ca8',
+        'reference' => '75b56e36e0b97ab23bc16d3530e3c1fd2d7b7e61',
         'type' => 'plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'jonathanlewis/fwbsite-iii' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'd83bcaefea809535047b0423b5eb7e4f56783ca8',
+            'reference' => '75b56e36e0b97ab23bc16d3530e3c1fd2d7b7e61',
             'type' => 'plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
