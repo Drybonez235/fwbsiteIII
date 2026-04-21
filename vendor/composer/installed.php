71cbeaf4d3ae2452b0c0f7d8cdccc72c4d0476f5
Jonathan Lewis <jonathan.lewis@welch.edu>
2026-04-21 14:59:15 -0500

    added nesting to the menu options

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'jonathanlewis/fwbsite-iii',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '9fc34c5b327e31867c1d5be5dc8077da91ebd9f3',
+        'reference' => 'b7beabc735cda6bc230af6530f082ebc16fd63f5',
         'type' => 'plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'jonathanlewis/fwbsite-iii' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '9fc34c5b327e31867c1d5be5dc8077da91ebd9f3',
+            'reference' => 'b7beabc735cda6bc230af6530f082ebc16fd63f5',
             'type' => 'plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
